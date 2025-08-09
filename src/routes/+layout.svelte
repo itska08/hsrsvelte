c5d4d0a7699bdfeafdddcffdb7b621860c9e13bd
itska08 <thaykhangday@gmail.com>
2025-08-09 22:28:38 +0700

    Add dark themed codex with login and admin panel

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,18 @@
 <script>
-	import "$styles/app.css";
-	import Header from "$components/Header.svelte";
+        import "$styles/app.css";
+        import Header from "$components/Header.svelte";
+        import { user } from "../stores/user.js";
+        import { onMount } from "svelte";
+        import { getUser } from "$utils/storage.js";
 
-	let { children } = $props();
+        let { children } = $props();
+
+        onMount(() => {
+                user.set(getUser());
+        });
 </script>
 
 <Header />
 <main id="content">
-	{@render children?.()}
+        {@render children?.()}
 </main>
